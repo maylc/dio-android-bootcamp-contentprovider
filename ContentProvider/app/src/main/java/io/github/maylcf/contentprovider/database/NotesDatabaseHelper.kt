16ce0507ce21fe81efbccec7606a9df97bfddf25
Mayara Felix <maylc.dev@gmail.com>
2021-09-12 23:15:34 -0400

    Implement functions in NotesProvider.

---
+++ ContentProvider/app/src/main/java/io/github/maylcf/contentprovider/database/NotesDatabaseHelper.kt
@@ -0,0 +1,27 @@
+package io.github.maylcf.contentprovider.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns._ID
+
+class NotesDatabaseHelper(context: Context) : SQLiteOpenHelper(context, null, 1) {
+
+    companion object {
+        const val TABLE_NOTES: String = "Notes"
+        const val TITLE_NOTES: String = "title"
+        const val DESCRIPTION_NOTES: String = "description"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(
+            "CREATE TABLE $TABLE_NOTES (" +
+                    "$_ID INTEGER NOT NULL PRIMARY KEY, " +
+                    "$TITLE_NOTES TEXT NOT NULL, " +
+                    "$DESCRIPTION_NOTES TEXT NOT NULL)"
+        )
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+    }
+}
